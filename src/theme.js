fab91f1d359a722b3e11e6a3bde6b0eb01f1e6b2
Damian Salawa <damiandv20@o2.pl>
2024-02-05 20:46:13 +0100

    add theme

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+
+export const theme = {
+    color: {
+        silver: "#eee",
+        white: "#ffffff",
+        teal: "#008080",
+        green: "#008000",
+        red: "#ff0000",
+    },
+    breakpoint: {
+        mobileMax: 767
+    },
+};
